155f04235897e54667e18ae545af3ef62fb370a2
Siiri Saar <siiri.saar1307@gmail.com>
2015-09-27 15:36:29 +0300

    Created Product JS class for handling requests

---
+++ src/main/webapp/product.js
@@ -0,0 +1,27 @@
+/**
+ * Created by siiri on 26/09/15.
+ */
+
+var application = angular.module('application', []);
+application.controller('productController', ['$scope','$http', '$window', function ($scope, $http, $window) {
+
+    $scope.saveData = function () {
+        console.log($scope.product);
+
+        if(!$scope.form.$valid) {
+            return;
+        }
+
+
+
+        $http.post('rest/products', $scope.product).
+            then(function(response) {
+
+                $window.location.href = 'rest/products';
+            }, function(response) {
+
+                console.error('Tekkis viga');
+            });
+    }
+}]);
+
