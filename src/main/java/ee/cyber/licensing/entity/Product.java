7510d3bde99587cb73a3d65975685fdc579d6805
Siiri Saar <siiri.saar1307@gmail.com>
2015-09-27 15:34:31 +0300

    Created Product java class

---
+++ src/main/java/ee/cyber/licensing/entity/Product.java
@@ -0,0 +1,43 @@
+package ee.cyber.licensing.entity;
+
+/**
+ * Created by siiri on 25/09/15.
+ */
+public class Product {
+
+    Integer id;
+    String name;
+    String release;
+
+    public Product(Integer id, String name, String release){
+        this.id = id;
+        this.name = name;
+        this.release = release;
+    }
+
+    public Product(){}
+   
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getRelease() {
+        return release;
+    }
+
+    public void setRelease(String release) {
+        this.release = release;
+    }
+}
