c5129a9c9c6778a02bf74f2785ddd598f2278999
Anu Lajal <anulajal@gmail.com>
2015-10-12 20:40:49 +0300

    Changed testSampleData.sql

--- src/main/resources/sampleData.sql
+++ src/main/resources/sampleData.sql
@@ -1,5 +1,5 @@
-INSERT INTO Product (name, release) VALUES ('MindShare', '11.2');
-INSERT INTO Product (name, release) VALUES ('i-Voting', '12.6');
+INSERT INTO Product (name, release) VALUES ('i-Voting', '11.2');
+INSERT INTO Product (name, release) VALUES ('ShareMind', '12.6');
 
 INSERT INTO LicenseOwner (name, address, webpage, registrationCode, phone, bankAccount, fax, unitOrFaculty)
 VALUES ('Example University', '123 Fake Street', 'www.example.com', '1A2B', '+372 555555', 'E100101', '+372 555555',
