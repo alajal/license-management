9e65c8b9a8f257a43bc8c39e71051b272c693751
Anu Lajal <anulajal@gmail.com>
2015-11-02 18:44:32 +0200

    Licenses view table fix Task: https://trello.com/c/9uAE00OF

--- src/main/webapp/components/license/licensesView.js
+++ src/main/webapp/components/license/licensesView.js
@@ -1,20 +1,13 @@
-// Add controller to module.
 angular
     .module('LM')
     .controller('ViewLicensesCtrl', function ($scope, $http) {
 
         $http.get('rest/licenses').
-            //server töötleb post päringut ja kui ta on sellega lõpetanud, siis minnakse siin alles edasi
             then(function (response) {
-                // this callback will be called asynchronously
-                // when the response is available
                 $scope.licenses = response.data;
 
             }, function (response) {
-                // called asynchronously if an error occurs
-                // or server returns response with an error status.
 
-                //nt kui päringus on midagi valesti - emailis on ainult numbrid
                 console.error('There was something wrong with the view licenses request.');
             });
 
